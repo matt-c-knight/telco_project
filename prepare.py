053050f87a792ebfa0839c7b557458d48cb94e38
Matthew Knight <mattknight.sa@gmail.com>
2020-09-22 11:49:40 -0500

    finalized prep function

--- prepare.py
+++ prepare.py
@@ -15,3 +15,29 @@ def telco_split(df):
                                         random_state=123,
                                         stratify=train_validate.churn)
     return train, validate, test
+
+
+def prep_telco(cached=True):
+
+    df = get_telco_data(cached)
+    df = df.drop(columns=['customer_id', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'internet_service_type_id', 'contract_type_id', 'payment_type_id', 'partner', 'dependents', 'multiple_lines', 'streaming_tv', 'streaming_movies', 'total_charges'])
+    df_dummies = pd.get_dummies(df.contract_type)
+    df = pd.concat([df, df_dummies], axis=1)
+    df = df.drop(columns='contract_type')
+    df_dummies = pd.get_dummies(df.internet_service_type)
+    df = pd.concat([df, df_dummies], axis=1)
+    df = df.drop(columns='internet_service_type')
+    df_dummies = pd.get_dummies(df.payment_type)
+    df = pd.concat([df, df_dummies], axis=1)
+    df = df.drop(columns='payment_type')
+    df.loc[df['phone_service'] == 'No', 'phone_service'] = 0
+    df.loc[df['phone_service'] == 'Yes', 'phone_service'] = 1
+    df.loc[df['paperless_billing'] == 'No', 'paperless_billing'] = 0
+    df.loc[df['paperless_billing'] == 'Yes', 'paperless_billing'] = 1
+    df.loc[df['churn'] == 'No', 'churn'] = 0
+    df.loc[df['churn'] == 'Yes', 'churn'] = 1
+    df_dummies = pd.get_dummies(df.gender)
+    df = pd.concat([df, df_dummies], axis=1)
+    df.drop(columns='gender')
+
+    return df
